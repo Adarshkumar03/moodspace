bc193f8250d7a2e102b8aef5ffdb27588537987b
Adarsh Kumar <adarshkumar245@gmail.com>
2024-05-24 22:43:30 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/v1": "https://moodspace-server.vercel.app/",
-    },
-  },
   css: {
     postcss: {
       plugins: [],
