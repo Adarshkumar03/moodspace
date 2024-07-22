49deb63f9615f0eaf7c7539d9128de5458fd5c2a
Adarsh Kumar <adarshkumar245@gmail.com>
2024-07-22 15:40:28 +0530

    update authStore

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -3,13 +3,13 @@ import { create } from "zustand";
 const useAuthStore = create((set) => ({
   isLoggedIn: false,
   token: null,
-  uname: null,
+  uname:null,
   uemail: null,
-  apiUrl: "https://moodspace-server.vercel.app/",
-  setName: (uname) => set({ uname }),
-  setUEmail: (uemail) => set({ uemail }),
+  apiUrl: "https://moodspace-server.vercel.app",
+  setName:(uname) => set({uname}),
+  setUEmail:(uemail) =>set({uemail}),  
   login: (token) => set({ token, isLoggedIn: true }),
-  logout: () => set({ token: null, isLoggedIn: false }),
+  logout: () => set({ token: null, isLoggedIn: false })
 }));
 
 export default useAuthStore;
