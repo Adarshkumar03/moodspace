b25cd3d0da40433c69b9d1579b8983667ed95d80
Adarsh Kumar <adarshkumar245@gmail.com>
2024-09-21 10:17:17 +0530

    update UserButton.jsx

--- src/components/UserButton/UserButton.jsx
+++ src/components/UserButton/UserButton.jsx
@@ -1,11 +1,9 @@
-import { UnstyledButton, Group, Avatar, Text, rem, Title } from "@mantine/core";
-import { IconChevronRight } from "@tabler/icons-react";
+import { UnstyledButton, Group, Avatar, Title } from "@mantine/core";
 import classes from "./UserButton.module.css";
 import useAuthStore from "../../stores/authStore";
 
 export function UserButton() {
   const uname = useAuthStore((store) => store.uname);
-  const uemail = useAuthStore((store) => store.uemail);
   return (
     <UnstyledButton className={classes.user} mb={10}>
       <Group>
